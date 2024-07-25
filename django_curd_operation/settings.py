5b66f4bdb5a2f4e503d730f91036979c66e8e2ba
bkartheek <164762327+kartheekbasa@users.noreply.github.com>
2024-07-25 19:47:05 +0530

    Update settings.py

--- django_curd_operation/settings.py
+++ django_curd_operation/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_curd_operation.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR/'templates'],
+        'DIRS': [BASE_DIR/'Templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
